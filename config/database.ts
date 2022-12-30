da442f3ccc5ae8870e728f16df9b068534c9ac3c
Oskar Vitko <ovitko@ibagroup.eu>
2022-12-30 18:34:13 +0300

    initial

---
+++ config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi-test'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'Oskar_vitko16'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
