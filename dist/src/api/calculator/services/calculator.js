5915acd22ada78eee6d6ad48eb044a868a71ee3c
oskarvitko <oskarvitko16@gmail.com>
2023-03-13 15:05:29 +0100

    universal calculator

---
+++ dist/src/api/calculator/services/calculator.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * calculator service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::calculator.calculator');
---
+++ dist/src/api/yandex-metrika/controllers/yandex-metrika.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * yandex-metrika controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::yandex-metrika.yandex-metrika');
